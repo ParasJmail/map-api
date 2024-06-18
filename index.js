772f3fe500a37626408cfb129882807991a7c3be
ParasJmail <116335733+ParasJmail@users.noreply.github.com>
2024-06-18 22:36:23 +0530

    backend added

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const pinRoute = require("./routes/pins");
+const userRoute = require("./routes/users");
+
+dotenv.config();
+
+
+
+const app = express();
+
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("mongodb connected")
+}).catch(err => console.log(err));
+
+app.use("/api/pins",pinRoute);
+app.use("/api/users",userRoute);
+
+app.listen(8800,()=>{
+    console.log("Backend server is running!")
+})
